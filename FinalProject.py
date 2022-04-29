b81ed94679216e7a00b4edb9aef5c9a3e29d95dd
spiasecki213 <spiaseck@terpmail.umd.edu>
2022-04-29 18:22:18 -0400

    added outline for program

---
+++ FinalProject.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+""" Create new address book window """
+
+# create list of contacts
+
+
+# list of variables for contact info
+
+
+""" Functions """
+
+# selected contact
+
+# add contact
+
+# edit contact
+
+# delete contact
+
+# view contact
+
+# close window
+
+# sort the list
+
+# if __name__ == main statement
