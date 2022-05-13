4b10e8c91f04ed047a710e891b8cc5558618f7b3
vanessanutella <33378653+vanessanutella@users.noreply.github.com>
2022-05-13 10:57:26 -0400

    testing edit_contact

--- test_address_book.py
+++ test_address_book.py
@@ -12,7 +12,16 @@ class TestMainWindow(unittest.TestCase):
         pass
 
     def test_edit_contact(self):
-        pass
+        """""Tests that the function accurately updates a name"""""
+        self.normal_fields()
+        new_fname = self.fname_strvar.get("John") #updated first name of the person is John
+        self.assertEqual(new_fname, "John") #checks that the new stored name is John
+        self.assertNotEqual(new_fname, "Josh") #checks that the new stored name is not the original name
+        assert new_fname is not None #checks that there is a value entered for the first name 
+        new_lname = self.lname_strvar.get("Doe") #updated last name of the person is Doe
+        self.assertEqual(new_lname, "Doe") #checks that the new stored name is Doe
+        self.assertNotEqual(new_lname, "Done") #checks that the new stored name is not the original name
+        assert new_lname is not None #checks that there is a value entered for the last name
 
     def test_list_contacts(self):
         pass
