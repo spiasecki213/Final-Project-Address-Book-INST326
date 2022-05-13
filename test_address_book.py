2c52500a6332c64311d203ba346150d9f5d6674d
vanessanutella <33378653+vanessanutella@users.noreply.github.com>
2022-05-13 00:46:17 -0400

    testing functions

--- test_address_book.py
+++ test_address_book.py
@@ -6,4 +6,40 @@ import sqlite3 as sql
 import address_book 
 import unittest
 
+class TestMainWindow(unittest.TestCase):
+    #This tests the add_contact method
+    def test_add_contact(self):
+        pass
 
+    def test_edit_contact(self):
+        pass
+
+    def test_list_contacts(self):
+        pass
+
+    def test_delete_contact(self):
+        pass
+
+    def test_delete_all_contacts(self):
+        pass
+
+    def test_view_contact(self):
+        pass
+
+    def test_clear_fields(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def test_read_only_fields(self):
+        pass
+
+    def test_normal_fields(self):
+        pass
+
+    def test__init__(self, root):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
